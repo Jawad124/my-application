a19f008e71d37f67119e0aef4601f23cdc7ee0ea
jawad <jawadahmedit6@gmail.com>
2020-03-29 12:19:26 +0500

    1st commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,24 +5,58 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Adapter;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private myadapter madapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<items> items_objects = new ArrayList<>();
-        items_objects.add(new items(R.drawable.ic_android,"heading 1","good"));
-        items_objects.add(new items(R.drawable.ic_android,"heading 2","good"));
-        items_objects.add(new items(R.drawable.ic_android,"heading 3","good"));
-        items_objects.add(new items(R.drawable.ic_android,"heading 4","good"));
+        items_objects.add(new items(R.drawable.ic_android,"jawad","good"));
+        items_objects.add(new items(R.drawable.ic_android,"rehan","good"));
+        items_objects.add(new items(R.drawable.ic_android,"atif","good"));
+        items_objects.add(new items(R.drawable.ic_android,"asghar","good"));
+        items_objects.add(new items(R.drawable.ic_android,"shaloom","good"));
+        items_objects.add(new items(R.drawable.ic_android,"arham","good"));
+        items_objects.add(new items(R.drawable.ic_android,"basit","good"));
+        items_objects.add(new items(R.drawable.ic_android,"ronaldo","good"));
 
         RecyclerView recycle_list = findViewById(R.id.rc);
         recycle_list.setLayoutManager(new LinearLayoutManager(this));
-        recycle_list.setAdapter(new myadapter(items_objects));
+         madapter =new myadapter(items_objects);
+        recycle_list.setAdapter(madapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_menu,menu);
+        MenuItem items = menu.findItem(R.id.searchView);
+
+        android.widget.SearchView searchView = (android.widget.SearchView) items.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_GO);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                madapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+        return true;
     }
 }
--- app/src/main/java/com/example/myapplication/myadapter.java
+++ app/src/main/java/com/example/myapplication/myadapter.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,15 +12,19 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.zip.Inflater;
 
-public class myadapter extends RecyclerView.Adapter<myadapter.viewholder> {
+public class myadapter extends RecyclerView.Adapter<myadapter.viewholder> implements Filterable {
 
    private ArrayList<items> mylist;
+   private ArrayList<items> backuplist=new ArrayList<>();
 
     public myadapter(ArrayList<items> mylist){
 
         this.mylist=mylist;
+        this.backuplist.addAll(mylist);
     }
 
 
@@ -44,7 +50,41 @@ public class myadapter extends RecyclerView.Adapter<myadapter.viewholder> {
     public int getItemCount() {
         return mylist.size();
     }
-    public class viewholder extends RecyclerView.ViewHolder{
+
+    @Override
+    public Filter getFilter() {
+        return itemsfilter;
+    }
+    private Filter itemsfilter =  new Filter() {
+        //it runs on background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<items> templist = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                templist.addAll(backuplist);
+            } else {
+                String filter = constraint.toString().toLowerCase().trim();
+                for (items loop : backuplist) {
+                    if (loop.gettxt1().contains(filter)) {
+                        templist.add(loop);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = templist;
+            return results;
+        }
+
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mylist.clear();
+            mylist.addAll((Collection<? extends items>) results.values);
+            notifyDataSetChanged();
+        }
+    };
+    class viewholder extends RecyclerView.ViewHolder{
        public ImageView imgsrc;
        public TextView t1;
        public TextView t2;
